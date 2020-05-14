87ec32e04a8f4a4bc1e0eab14ec0af3398441eb5
Ischi94 <gmathes94@gmx.de>
2020-05-14 11:38:32 +0200

    windows update.

--- week2_exercise.R
+++ week2_exercise.R
@@ -1,6 +1,10 @@
 library(tidyverse)
 library(rethinking)
 
+
+# Chapter 4 ---------------------------------------------------------------
+
+
 # data
 data("Howell1")
 Howell1 <- Howell1 %>% as_tibble()
@@ -20,3 +24,36 @@ ggplot(prior_y) +
 
 # 4M2
 # Translate the model just above into a map() formula
+map_frm <- alist(y ~ dnorm(mu, sigma), 
+                 mu ~ dnorm(0, 10), 
+                 sigma ~ dexp(1)) 
+
+# 4M3
+# Translate the map() model formula below into a mathematical model definition.
+flist <- alist(
+  y ~ dnorm(mu, sigma),
+  mu <- a + b*x,
+  a ~ dnorm(0, 50),
+  b ~ dunif(0, 10),
+  sigma ~ dunif(0, 50)
+)
+
+# y1 ~ Normal(μ,σ) / μ ~ a + Bx1 / a ~ Normal(0, 50) / 
+# B = Uniform(0, 10) / σ ~ Uniform(0, 50) 
+
+# 4M4
+# A sample of students is measured for height each year for 3 years.
+# After the third year, you want to fit a linear regression predicting height using 
+# year as a predictor. Write down the mathematical model definition for this regression, 
+# using any variable names and priors you choose. 
+# Be prepared to defend your choice of priors.
+
+# height ~ Normal(μ,σ) / μ1 ~ a + Bx1 / a ~ Normal(150, 25) / 
+# B = Uniform(4, 2) / σ ~ Exponential(1)
+
+# 4M5
+# Now suppose I tell you that the average height in the first year was 120 cm and 
+# that every student got taller each year. Does this information lead you to change 
+# your choice of priors? How?
+# height ~ Normal(μ,σ) / μ1 ~ a + Bx1 / a ~ Normal(120, 10) / 
+# B = Uniform(7, 1) / σ ~ Exponential(1)
