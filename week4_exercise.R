f67cbac80d1916f25fbcb4e034d9b4fc7ed39468
Ischi94 <gmathes94@gmx.de>
2021-02-01 16:56:06 +0100

    finnish 6H2

--- week4_exercise.R
+++ week4_exercise.R
@@ -276,3 +276,165 @@ tribble(
 # which we want to keep as true causal.
 
 
+### 6H1 ###
+# Use the Waffle House data, data(WaffleDivorce), to find the total causal
+# influence of number of Waffle Houses on divorce rate. Justify your model or
+# models with a causal graph.
+
+data("WaffleDivorce")
+
+dat_waffle <- WaffleDivorce %>% 
+  as_tibble() %>% 
+  mutate(across(where(is.numeric), standardize)) %>% 
+  select(divorce = Divorce, age = MedianAgeMarriage,
+         m_rate = Marriage, waffle = WaffleHouses,
+         south = South)
+
+# divorce <- age -> m_rate
+# waffle <- south -> age
+
+tribble(
+  ~ name,    ~ x,  ~ y,  
+  "south",     0,    2,     
+  "age",       1,    2,     
+  "m_rate",    2,    1,     
+  "waffle",    0,    0, 
+  "divorce",   1,    0
+) %>%  
+  dagify(
+    divorce ~ age + waffle,
+    waffle ~ south, 
+    age ~ south,
+    m_rate ~ age,
+    coords = .) %>% 
+  ggplot(aes(x = x, y = y, xend = xend, yend = yend)) +
+  geom_dag_node(colour = purple, shape = 1, size = 20) +
+  geom_dag_text(aes(label = name), color = "black", size = 4) +
+  geom_dag_edges(edge_color = coral) +
+  labs(caption = "Figure 6: The total causal influence of number of Waffle Houses on divorce rate") +
+  theme_void()
+
+m_waffle <- alist(
+  divorce ~ dnorm(mu, sigma),
+  mu <- a + Bwaffle * waffle + Bsouth * south,
+  a ~ dnorm(0, 0.2),
+  c(Bwaffle, Bsouth) ~ dnorm(0, 0.5),
+  sigma ~ dexp(1)) %>% 
+  quap(data = dat_waffle) 
+
+
+s <- seq(from = -2, to = 2, length.out = 30)
+N <- 1e3
+
+m_waffle %>% 
+  link(data = list(waffle = s, 
+                   south = 0)) %>% 
+  as_tibble() %>% 
+  pivot_longer(cols = everything(), values_to = "pred_divorce") %>% 
+  add_column(waffle = rep(s, N)) %>% 
+  group_by(waffle) %>% 
+  nest() %>% 
+  mutate(pred_divorce = map(data, "pred_divorce"), 
+         mean_divorce = map_dbl(pred_divorce, mean), 
+         pi = map(pred_divorce, PI), 
+         lower_pi = map_dbl(pi, pluck(1)), 
+         upper_pi = map_dbl(pi, pluck(2))) %>% 
+  select(waffle, mean_divorce, lower_pi, upper_pi) %>% 
+  ggplot() +
+  geom_ribbon(aes(waffle, ymin = lower_pi, ymax = upper_pi), 
+              fill = mint, alpha = 0.3) +
+  geom_line(aes(waffle, mean_divorce), 
+            size = 1.5, colour = coral) +
+  coord_cartesian(ylim = c(-2, 2)) +
+  labs(x = "Waffle (std)", y = "Divorce (std)", 
+       caption = "Figure 7: Total causal effect of number of Waffle House on Divorce, 
+       while keeping 'South' constant.") +
+  theme_minimal()
+
+### 6H2 ###
+# Build a series of models to test the implied conditional independencies of the
+# causal graph you used in the previous problem. If any of the tests fail, how
+# do you think the graph needs to be amended? Does the graph need more or fewer
+# arrows? Feel free to nominate variables that are not in the data.
+
+dagitty("dag{
+        waffle -> divorce <- age <- south -> waffle
+        age -> m_rate}") %>% 
+  impliedConditionalIndependencies()
+
+
+# age _||_ waffle | south
+alist(
+  waffle ~ dnorm(mu, sigma),
+  mu <- a + Bsouth*south + Bage*age,
+  a ~ dnorm(0, 0.2),
+  c(Bsouth, Bage) ~ dnorm(0, 0.5),
+  sigma ~ dexp(1)) %>% 
+  quap(data = dat_waffle) %>% 
+  precis() %>% 
+  as_tibble(rownames = "estimate") %>% 
+  filter(estimate == "Bage") %>% 
+  mutate(across(where(is.numeric), round, digits = 2)) %>% 
+  knitr::kable()
+
+test_independence <- function(model.input, coeff.input){
+  suppressWarnings(
+    precis(model.input) %>%
+    as_tibble(rownames = "estimate") %>%
+    filter(estimate == {{coeff.input}}) %>%
+    mutate(across(where(is.numeric), round, digits = 2)) %>%
+    knitr::kable()
+  )
+}
+
+# divorce _||_ m_rate | age
+alist(
+  m_rate ~ dnorm(mu, sigma),
+  mu <- a + Bdivorce*divorce + Bage*age,
+  a ~ dnorm(0, 0.2),
+  c(Bdivorce, Bage) ~ dnorm(0, 0.5),
+  sigma ~ dexp(1)) %>% 
+  quap(data = dat_waffle) %>% 
+  test_independence(coeff.input = "Bdivorce")
+  
+
+# divorce _||_ south | age, waffle
+alist(
+  south ~ dnorm(mu, sigma),
+  mu <- a + Bdivorce*divorce + Bage*age + Bwaffle*waffle,
+  a ~ dnorm(0, 0.2),
+  c(Bdivorce, Bage, Bwaffle) ~ dnorm(0, 0.5),
+  sigma ~ dexp(1)) %>% 
+  quap(data = dat_waffle) %>% 
+  test_independence(coeff.input = "Bdivorce")
+
+# m_rate _||_ south | age
+alist(
+  south ~ dnorm(mu, sigma),
+  mu <- a + Bm_rate*m_rate + Bage*age,
+  a ~ dnorm(0, 0.2),
+  c(Bm_rate, Bage) ~ dnorm(0, 0.5),
+  sigma ~ dexp(1)) %>% 
+  quap(data = dat_waffle) %>% 
+  test_independence(coeff.input = "Bm_rate")
+
+# m_rate _||_ waffle | south
+alist(
+  waffle ~ dnorm(mu, sigma),
+  mu <- a + Bm_rate*m_rate + Bsouth*south,
+  a ~ dnorm(0, 0.2),
+  c(Bm_rate, Bsouth) ~ dnorm(0, 0.5),
+  sigma ~ dexp(1)) %>% 
+  quap(data = dat_waffle) %>% 
+  test_independence(coeff.input = "Bm_rate")
+
+# m_rate _||_ waffle | age
+alist(
+  waffle ~ dnorm(mu, sigma),
+  mu <- a + Bm_rate*m_rate + Bage*age,
+  a ~ dnorm(0, 0.2),
+  c(Bm_rate, Bage) ~ dnorm(0, 0.5),
+  sigma ~ dexp(1)) %>% 
+  quap(data = dat_waffle) %>% 
+  test_independence(coeff.input = "Bm_rate")
+
